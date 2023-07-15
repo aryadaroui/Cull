6b4ec701e1ff41d34219b961560ceb8e0fcf02d3
aryadaroui <adaroui@uci.edu>
2023-07-14 20:16:26 -0700

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import Greet from "./lib/Greet.svelte";
-
   import { open } from "@tauri-apps/api/dialog";
-  // import { appDir } from "@tauri-apps/api/path";
   import { readDir, BaseDirectory } from "@tauri-apps/api/fs";
   import { convertFileSrc } from "@tauri-apps/api/tauri";
   import { invoke } from "@tauri-apps/api/tauri";
@@ -23,44 +20,59 @@
       console.log("read dir: ", read_dir);
 
       const entries = await readDir(read_dir, { recursive: false });
-      // console.log("entries: ", entries);
-
       entries.forEach((entry) => {
         console.log("entry: ", entry);
       });
     });
   }
 
+  let viewer: ImageViewer;
 
-  // works but doesnt zoom on cursor position
   onMount(() => {
-    
-    const viewer = new ImageViewer(canvas);
+    viewer = new ImageViewer(canvas);
 
     // call the reset() function when the user presses the enter key
     window.addEventListener("keydown", (e) => {
       if (e.key === "Enter") {
         viewer.reset();
       }
-    });
 
+      if (e.key === "o" && e.metaKey) {
+        choose_dir();
+      }
+
+      if (e.key === "ArrowRight") {
+        // viewer.next();
+        console.log("next");
+      }
+
+      if (e.key === "ArrowLeft") {
+        // viewer.prev();
+        console.log("prev");
+      }
+    });
   });
 </script>
 
 <main id="windowframe">
+  <canvas bind:this={canvas} />
   <div id="toolbar">
     <button id="choose-dir" on:click={choose_dir}> Choose folder</button>
+    <button
+      id="choose-dir"
+      on:click={() => {
+        viewer.reset();
+      }}
+    >
+      Reset</button
+    >
   </div>
-  <canvas bind:this={canvas} />
+
   <div id="reel" />
 </main>
 
 <style lang="scss">
-
-
-
   main#windowframe {
-    
     display: flex;
     flex-direction: column;
     height: 100vh;
@@ -73,6 +85,7 @@
       align-items: center;
       height: 30px;
       width: calc(100vw - 2px);
+      background-color: rgba(37, 37, 37, 1);
 
       button#choose-dir {
         max-width: 200px;
@@ -92,7 +105,7 @@
     }
 
     div#reel {
-      background-color: rgba(40, 40, 40, 1.0);
+      background-color: rgba(40, 40, 40, 1);
       height: 200px;
       width: calc(100vw - 2px);
     }
