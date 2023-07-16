9aba84f0c09f8d4dc0874e0b5a032e38e2df04e2
aryadaroui <adaroui@uci.edu>
2023-07-16 14:50:32 -0700

    center marker for reel

--- src/Reel.svelte
+++ src/Reel.svelte
@@ -17,6 +17,7 @@
 </script>
 
 <div id="reel">
+  <div id="pad" />
   {#await fetchData()}
     <p>loading</p>
   {:then items}
@@ -28,23 +29,47 @@
   {:catch error}
     <p style="color: red">{error.message}</p>
   {/await}
+
+  <div id="center-marker" />
 </div>
 
 <style lang="scss">
+  #reel {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    // justify-content: center;
+    align-items: center;
 
-	#reel {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-items: center;
+    height: 200px;
+    flex-wrap: nowrap;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    // height: 100vh;
+    // width: 100vw;
+    background-color: rgba(32, 32, 32, 0.7);
 
-		height: 200px;
-		flex-wrap: nowrap;
-		overflow-x: scroll;
-		overflow-y: hidden;
-		// height: 100vh;
-		// width: 100vw;
-		background-color: rgba(32, 32, 32, 0.7);
-	}
+    scroll-snap-type: x mandatory;
+
+    #pad {
+      width: 50px;
+      height: 200px;
+      background-color: rgba(0, 0, 0, 1);
+    }
+
+    img {
+      max-height: 150px;
+      scroll-snap-align: center;
+      cursor: pointer;
+      // scroll-margin-left: 50vw;
+    }
+
+    #center-marker {
+      position: absolute;
+      left: 50vw;
+      width: 1px;
+      height: 200px;
+      border: 1px solid red;
+    }
+  }
 </style>
