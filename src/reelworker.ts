d4b9b77f25b0aa3a43f520fb2b40fc4222a862dc
aryadaroui <adaroui@uci.edu>
2023-07-17 11:14:43 -0700

    clean reelworker

--- src/reelworker.ts
+++ src/reelworker.ts
@@ -2,9 +2,6 @@ import ImageBlobReduce from "image-blob-reduce";
 
 const reducer = new ImageBlobReduce();
 
-// const reducer = require('image-blob-reduce')();
-
-
 onmessage = function (e) {
 	fetch(e.data[0]).then((r) => {
 		console.log(r);
